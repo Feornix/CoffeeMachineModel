38100f8d84e43317d8f2b0187558888db3a52ad9
griml <grimlock.pheornix@bk.ru>
2025-04-09 00:00:28 +0600

    Создание интерфейса ICoffeeMachine.

---
+++ CoffeeMachineModel.Core/ICoffeeMachine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachineModel.Core
+{
+    interface ICoffeeMachine
+    {
+        Coffee BrewCoffee(string name);
+    }
+}
