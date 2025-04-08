5a735562edee32e3d1cfd2a4b9110f7b3cbb7f37
griml <grimlock.pheornix@bk.ru>
2025-04-08 23:54:55 +0600

    Создание класса Coffee с конструктором.

---
+++ CoffeeMachineModel.Core/Coffee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachineModel.Core
+{
+    class Coffee
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (value == "")
+                    throw new ArgumentException("Coffee name cannot be empty");
+                name = value;
+            }
+        }
+        public Coffee(string name)
+        {
+            Name = name;
+        }
+    }
+}
