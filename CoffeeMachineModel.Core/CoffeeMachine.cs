e2a2a5818789dc5e318603d5559b3e20d3e1f30f
griml <grimlock.pheornix@bk.ru>
2025-04-09 00:07:07 +0600

    Создание класса CoffeeMachine.

---
+++ CoffeeMachineModel.Core/CoffeeMachine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachineModel.Core
+{
+    class CoffeeMachine: ICoffeeMachine
+    {
+        public List<CoffeeRecipe> CoffeeRecipes { get; private set; }
+        public Dictionary<string, int> Resources { get; private set; }
+    }
+}
